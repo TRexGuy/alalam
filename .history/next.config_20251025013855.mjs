615bcf9c1af10222f2b8909328dd125c001d6b6b
T-Rex <Trex.dev@hotmail.com>
2025-10-25 01:40:37 +0330

    Fixed Vercel Bog

--- .history/next.config_20251025013855.mjs
+++ .history/next.config_20251025013855.mjs
@@ -2,8 +2,10 @@ import path from "path";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // اگر خروجی استاتیک لازم داری، نگهش دار. در غیر این صورت می‌تونی حذفش کنی.
   output: "export",
 
+  // صراحتاً به Next.js بگو از Webpack استفاده کنه (نه Turbopack)
   turbopack: {},
 
   webpack(config) {
